a303f735ff7eabb0a52c2a71bf3bb439b3f371f6
Dan Wagoner <dan.wagoner@mailprotector.com>
2021-11-02 14:46:42 -0400

    updated confs to use jinja template

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,12 @@ LABEL name="rspamd"
 # install rspamd and pre-reqs
 RUN echo '@community http://mirror.clarkson.edu/alpine/latest-stable/community/' >> /etc/apk/repositories && \
     apk update && \
-    apk add --no-cache rspamd bash tini
+    apk add --no-cache rspamd rspamd-controller rspamd-proxy bash tini
 
 COPY entrypoint.sh /entrypoint.sh
 COPY conf/ /etc/rspamd
+RUN mkdir /run/rspamd
 
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["tini", "/usr/sbin/rspamd", "-i", "-f"]
-EXPOSE 11331
+EXPOSE 11334
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,4 @@
-#!/bin/bash
-set -m
-
-REDIS_PORT=$(env | grep "REDIS_PORT" | awk -F "=" '{print $2}')
-REDIS_WRITE=$(env | grep "REDIS_WRITE" | awk -F "=" '{print $2}')
-REDIS_READ=$(env | grep "REDIS_READ" | awk -F "=" '{print $2}')
-CLAMAV_HOST=$(env | grep "CLAMAV_HOST" | awk -F "=" '{print $2}')
-
-if [ -z $REDIS_PORT ]
-then
-  REDIS_PORT="6379"
-fi
-
-if [ -z $REDIS_WRITE ]
-then 
-  REDIS_WRITE="redis"
-fi
-
-echo "entrypoint: setting redis write node - $REDIS_WRITE:$REDIS_PORT"
-sed -i "s|REDIS_WRITE|$REDIS_WRITE:$REDIS_PORT|g" "/etc/rspamd/local.d/redis.conf"
-
-if [ ! -z $REDIS_READ ]
-then
-  echo "entrypoint: setting redis read node - $REDIS_READ:$REDIS_PORT"
-  echo "read_servers = "$REDIS_READ:$REDIS_PORT";" | cat >> "/etc/rspamd/local.d/redis.conf"
-fi
-
-if [ ! -z $CLAMAV_HOST ]
-then
-  echo "entrypoint: enabling clamav and setting host - $CLAMAV_HOST"
-  cat /etc/rspamd/temp/clamav.conf >> /etc/rspamd/local.d/antivirus.conf
-  sed -i "s|CLAMAV_HOST|$CLAMAV_HOST|g" "/etc/rspamd/local.d/antivirus.conf"
-fi
+#!/bin/sh
 
 LOGFILE="/var/log/rspamd/rspamd.log"
 
