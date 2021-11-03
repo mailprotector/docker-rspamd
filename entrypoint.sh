24c74e65c1a3f1920ace737299a461176a6124ed
Dan Wagoner <dan.wagoner@mailprotector.com>
2021-11-03 12:08:05 -0400

    modified all configs to use jinja templated approach

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-LOGFILE="/var/log/rspamd/rspamd.log"
+# LOGFILE="/var/log/rspamd/rspamd.log"
 
-mkdir -p /var/log/rspamd
-if [ ! -f $LOGFILE ]; then
-    touch $LOGFILE
-    chown rspamd:rspamd $LOGFILE
-fi
+# mkdir -p /var/log/rspamd
+# if [ ! -f $LOGFILE ]; then
+#     touch $LOGFILE
+#     chown rspamd:rspamd $LOGFILE
+# fi
 
-exec tail -f $LOGFILE &
+# exec tail -f $LOGFILE &
 exec "$@"
